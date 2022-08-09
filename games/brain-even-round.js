766860e9910ccaefb3a076e577041633d1a3d0cd
Khrom24 <97703278+Khrom24@users.noreply.github.com>
2022-08-09 15:48:54 +0300

    Add files via upload

--- games/brain-even-round.js
+++ games/brain-even-round.js
@@ -1,11 +1,11 @@
 import readlineSync from 'readline-sync';
 
 const brainCalcRound = () => {
-  const randomNum = Math.round(Math.random() * 100); //рандомное число
-  const correctAnswer = randomNum % 2 === 0 ? 'yes' : 'no'; //проверяем, четное ли оно
+  const randomNum = Math.round(Math.random() * 100); // рандомное число
+  const correctAnswer = randomNum % 2 === 0 ? 'yes' : 'no'; // проверяем, четное ли оно
   console.log(`Question: ${randomNum}`);
   const answer = readlineSync.question('Your answer: ');
-  if (answer === correctAnswer) { //сравниваем ответы
+  if (answer === correctAnswer) { // сравниваем ответы
     console.log('Correct!');
     return 'correct';
   }
--- games/brain-progression-round.js
+++ games/brain-progression-round.js
@@ -1,10 +1,10 @@
 import readlineSync from 'readline-sync';
 
 const brainCalcRound = () => {
-  const noOfNums = Math.round(Math.random() * 5) + 5; //колличество (если умножить на 10, то найти ответ будет легко)
-  const missingNum = Math.round(Math.random() * noOfNums); //отсутсвующая цифра
-  const firstNum = Math.round(Math.random() * 100); //первый номер
-  const progressionStep = Math.round(Math.random() * 10); //шаг
+  const noOfNums = Math.round(Math.random() * 5) + 5; // колличество
+  const missingNum = Math.round(Math.random() * noOfNums); // отсутсвующая цифра
+  const firstNum = Math.round(Math.random() * 100); // первый номер
+  const progressionStep = Math.round(Math.random() * 10); // шаг
   const correctAnswer = firstNum + progressionStep * (missingNum - 1);
   const array = [];
   for (let i = 0; i < noOfNums; i += 1) {
@@ -14,7 +14,7 @@ const brainCalcRound = () => {
       array[i] = firstNum + progressionStep * i;
     }
   }
-  const sequence = array.join(' '); //разъединяем пробелами
+  const sequence = array.join(' '); // разъединяем пробелами
   console.log(`Question: ${sequence}`);
   const answer = readlineSync.question('Your answer: ');
   if (Number(answer) === correctAnswer) {
